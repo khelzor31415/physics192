ac9d748ef0b044f2d330b46c6f8141ccba0e0799
Ian Angelo Aragoza <khelzor31415@gmail.com>
2021-10-28 13:57:50 +0800

    1

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+print('imported modules')
+
+
+df = pd.read_csv("C:\Users\Ian Angelo\OneDrive\Desktop\Coding\physics192\physics192\data\last.csv")
+print(df.head())
---
+++ test.py
@@ -0,0 +1,3 @@
+s = input('put text here:\n')
+print(s)
+import pandas as pd
